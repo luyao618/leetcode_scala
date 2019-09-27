77d51a0bcb8d9201097428f977f2a7c9a43a433b
luyao3 <luyao3@xiaomi.com>
2019-09-27 14:17:48 +0800

    test git

---
+++ src/main/scala/canReorderDoubled_954.scala
@@ -0,0 +1,15 @@
+/**
+  *
+  * @auther luyao
+  * @date 19-9-27
+  * @version 1.0
+  */
+object canReorderDoubled_954 {
+
+  // 也就是说对与给定的数组,可以分成len(A)/2对数据,每对数是a,2a
+  // 先排序,然后找大数
+  def canReorderDoubled(A: Array[Int]): Boolean = {
+    false
+  }
+
+}
